e76a0b9c35ad651bdd8b98bf22eb589155993a48
maseology <maseology@pm.me>
2019-03-21 19:26:34 -0400

    fatal error was unecssary in FileExists

--- files.go
+++ files.go
@@ -31,7 +31,15 @@ func FileExists(fp string) (int64, bool) {
 	} else if os.IsNotExist(err) {
 		return 0, false
 	} else {
-		log.Fatal(err)
+		// log.Fatalf("mmio.FileExists: %v", err)
 		return 0, false
 	}
+}
+
+// DirExists checks if a directory exists
+func DirExists(path string) bool {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return false
+	}
+	return true
 }
