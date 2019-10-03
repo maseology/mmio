666937f03f676755b9b99aa3797b6d74e9fe2bde
maseology <mason@riffle.ca>
2019-10-03 16:46:05 -0400

    update ascii

--- ascii.go
+++ ascii.go
@@ -12,13 +12,29 @@ func WriteInts(fp string, d []int) error {
 	if err != nil {
 		return err
 	}
-
 	for _, v := range d {
 		if _, err := f.Write([]byte(fmt.Sprintf("%d\n", v))); err != nil {
 			return err
 		}
 	}
+	if err := f.Close(); err != nil {
+		return err
+	}
+	return nil
+}
 
+// WriteFloats is a simple routine that writes an float slice to an ascii file
+func WriteFloats(fp string, d []float64) error {
+	f, err := os.OpenFile(fp, os.O_CREATE|os.O_WRONLY, 0644)
+	// f, err := os.OpenFile(fp, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644) // append
+	if err != nil {
+		return err
+	}
+	for _, v := range d {
+		if _, err := f.Write([]byte(fmt.Sprintf("%f\n", v))); err != nil {
+			return err
+		}
+	}
 	if err := f.Close(); err != nil {
 		return err
 	}
