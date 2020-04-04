58b4bbfe9e90b8c9679ba99e84debbea3cafe5f7
maseology <mason@riffle.ca>
2020-04-03 20:57:04 -0400

    update 200403

--- plotters.go
+++ plotters.go
@@ -420,9 +420,9 @@ func sequentialLine(v []float64) plotter.XYs {
 }
 
 func cumulativeDistributionLine(v []float64) plotter.XYs {
-	v = OnlyPositive(v)
+	v = onlyPositive(v)
 	sort.Float64s(v)
-	RevF(v)
+	revF(v)
 	pts, c, x := make(plotter.XYs, len(v)), 0, float64(len(v))/100.
 	for i := range pts {
 		if math.IsNaN(v[i]) {
@@ -433,4 +433,33 @@ func cumulativeDistributionLine(v []float64) plotter.XYs {
 		c++
 	}
 	return pts[:c]
+}
+
+// rev is quick function used to reverse order of a slice
+func rev(s []int) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
+
+// revF is quick function used to reverse order of a float64 slice
+func revF(s []float64) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
+
+// onlyPositive removes all value <= 0.0 and all NaN's
+func onlyPositive(s []float64) []float64 {
+	var x []int
+	for i := range s {
+		if s[i] <= 0 || math.IsNaN(s[i]) {
+			x = append(x, i)
+		}
+	}
+	rev(x)
+	for _, i := range x {
+		s = append(s[:i], s[i+1:]...)
+	}
+	return s[:len(s)]
 }
