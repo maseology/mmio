f2bc07c87928a8f1e33106c73ed95a6d5acee30f
maseology <mason@riffle.ca>
2021-01-10 11:35:07 -0500

    ReadCsvDateFloat

--- csvSpecial.go
+++ csvSpecial.go
@@ -8,8 +8,8 @@ import (
 	"time"
 )
 
-// ReadCsvDateValueFlag reads temporal csv file "date,value,flag"
-func ReadCsvDateValueFlag(csvfp string) (map[time.Time]float64, error) {
+// ReadCsvDateFloat reads temporal csv file "date,value,flag,..."
+func ReadCsvDateFloat(csvfp string) (map[time.Time]float64, error) {
 	f, err := os.Open(csvfp)
 	if err != nil {
 		fmt.Printf("ReadCSV failed: %v\n", err)
@@ -24,7 +24,10 @@ func ReadCsvDateValueFlag(csvfp string) (map[time.Time]float64, error) {
 		if err != nil {
 			t, err = time.Parse("2006-01-02T15:04:05", rec[0])
 			if err != nil {
-				return nil, fmt.Errorf("date parse error: %v", err)
+				t, err = time.Parse("2006-01-02 15:04:05 +0000 UTC", rec[0])
+				if err != nil {
+					return nil, fmt.Errorf("date parse error in %s: %v", csvfp, err)
+				}
 			}
 		}
 		v, err := strconv.ParseFloat(rec[1], 64)
@@ -32,6 +35,48 @@ func ReadCsvDateValueFlag(csvfp string) (map[time.Time]float64, error) {
 			return nil, fmt.Errorf("value parse error: %v", err)
 		}
 		o[t] = v
+	}
+	return o, nil
+}
+
+// ReadCsvStringInt reads temporal csv file ith column type "<str>,<int>"
+func ReadCsvStringInt(csvfp string) (map[string]int, error) {
+	f, err := os.Open(csvfp)
+	if err != nil {
+		fmt.Printf("ReadCSV failed: %v\n", err)
+		return nil, fmt.Errorf("ReadCSV failed: %v", err)
+	}
+	defer f.Close()
+
+	recs := LoadCSV(io.Reader(f))
+	o := make(map[string]int, len(recs)-1)
+	for rec := range recs {
+		v, err := strconv.Atoi(rec[1])
+		if err != nil {
+			return nil, fmt.Errorf("value parse error: %v", err)
+		}
+		o[rec[0]] = v
+	}
+	return o, nil
+}
+
+// ReadCsvStringFloat reads temporal csv file ith column type "<str>,<float>"
+func ReadCsvStringFloat(csvfp string) (map[string]float64, error) {
+	f, err := os.Open(csvfp)
+	if err != nil {
+		fmt.Printf("ReadCSV failed: %v\n", err)
+		return nil, fmt.Errorf("ReadCSV failed: %v", err)
+	}
+	defer f.Close()
+
+	recs := LoadCSV(io.Reader(f))
+	o := make(map[string]float64, len(recs)-1)
+	for rec := range recs {
+		v, err := strconv.ParseFloat(rec[1], 64)
+		if err != nil {
+			return nil, fmt.Errorf("value parse error: %v", err)
+		}
+		o[rec[0]] = v
 	}
 	return o, nil
 }
