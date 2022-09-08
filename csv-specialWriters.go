0496a9ff0bbebb919cf2f717104756cc5363c32d
maseology <maseology@pm.me>
2022-09-08 11:29:54 -0400

    u

--- csv-specialWriters.go
+++ csv-specialWriters.go
@@ -2,6 +2,26 @@ package mmio
 
 import "time"
 
+func WriteCsvFloats(csvfp, header string, d ...[]float64) error {
+	csv := NewCSVwriter(csvfp)
+	defer csv.Close()
+	if err := csv.WriteHead(header); err != nil {
+		return err
+	}
+	nc := len(d)
+	nr := len(d[0])
+	for i := 0; i < nr; i++ {
+		iv := make([]interface{}, nc)
+		for j := 0; j < nc; j++ {
+			iv[j] = d[j][i]
+		}
+		if err := csv.WriteLine(iv...); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func WriteCsvDateFloats(csvfp, header string, t []time.Time, d ...[]float64) error {
 	csv := NewCSVwriter(csvfp)
 	defer csv.Close()
